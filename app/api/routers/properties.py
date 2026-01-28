26bf04ee8bac7f3a617f7fdba1f8f480ff6a35d5
Ihimbru-k <kanihims25@gmail.com>
2026-01-28 02:00:43 +0100

    Added GET /properties and /tenants enpoints + fix DB pooling

--- app/api/routers/properties.py
+++ app/api/routers/properties.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+
+from app import models
 from ... import schemas, crud
 from ...database import get_db
 from ...utils.auth import get_current_landlord_id
@@ -14,7 +16,19 @@ def create_property(
 ):
     return crud.create_property(db, property, landlord_id)
 
-
+# Added enpoint to Get all properties for landlord
+@router.get("/", response_model=list[schemas.PropertyResponse])
+def get_properties(
+    landlord_id: int = Depends(get_current_landlord_id),
+    db: Session = Depends(get_db),
+    skip: int = 0,
+    limit: int = 100
+):
+    properties = db.query(models.Property).filter(
+        models.Property.landlord_id == landlord_id,
+        models.Property.is_active == True
+    ).offset(skip).limit(limit).all()
+    return properties
 
 
 
--- app/database.py
+++ app/database.py
@@ -1,14 +1,32 @@
-from sqlalchemy import create_engine
+# app/database.py
+from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+# CONNECTION POOL SETTINGS
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,  # Test connections before using
+    pool_recycle=3600,   #Recycle connections every hour
+    pool_size=5,         # Max 5 connections
+    max_overflow=10,     # Allow 10 extra connections
+    connect_args={
+        "connect_timeout": 10,
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5,
+    }
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
 def get_db():
@@ -17,3 +35,45 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
